10061fffe0c8207fe40e4ca7ead5322fd1f7442a
Anson VanDoren <anson.vandoren@gmail.com>
2019-11-23 14:02:54 -0800

    moving functions to consistent location

--- .env.sh
+++ .env.sh
@@ -5,6 +5,11 @@ export EDITOR=vim
 # Create a folder and move into it in one command
 function mkd() { mkdir -p "$@" && cd "$_"; }
 
+# fh - repeat history
+fh() {
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -r 's/ *[0-9]*\*? *//' | sed -r 's/\\/\\\\/g')
+}
+
 # Aliases
 source ~/.aliases
 
--- .zshrc
+++ .zshrc
@@ -134,8 +134,3 @@ fi
 
 # use vim keybindings in shell
 bindkey -v
-# fh - repeat history
-fh() {
-  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -r 's/ *[0-9]*\*? *//' | sed -r 's/\\/\\\\/g')
-}
-
