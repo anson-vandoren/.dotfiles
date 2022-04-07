da53b0e9daa945373cc629f9c2e05305adc095f6
Anson VanDoren <anson.vandoren@gmail.com>
2022-04-07 09:23:40 -0700

    adds proxy scripts

---
+++ dockersetproxy.sh
@@ -0,0 +1,5 @@
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+export HTTP_PROXY="http://host.docker.internal:3128"
+export http_proxy="http://host.docker.internal:3128"
+export HTTPS_PROXY="http://host.docker.internal:3128"
+export https_proxy="http://host.docker.internal:3128"
---
+++ unsetproxy.sh
@@ -0,0 +1,5 @@
+unset NODE_TLS_REJECT_UNAUTHORIZED
+unset HTTP_PROXY
+unset http_proxy
+unset HTTPS_PROXY
+unset https_proxy
