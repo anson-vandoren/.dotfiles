50fe399c603425a1a7e271b13cee1cd99fb89ec6
Anson VanDoren <anson.vandoren@gmail.com>
2022-08-23 06:40:49 -0700

    sway launcher & logout

--- installStuff.sh
+++ installStuff.sh
@@ -1,2 +1,6 @@
-sudo pacman -S neovim exa dog curlie fd bat
-yay -S gotop tre-command
+sudo pacman -S neovim exa dog curlie fd bat alacritty qutebrowser jq unzip fzf pv
+yay -S gotop tre-command 1password wlogout
+
+echo "TODO:"
+echo "- Install 1Password CLI: https://developer.1password.com/docs/cli/get-started#install"
+
