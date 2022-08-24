ed088435113c4ba82c199e19a7f9231d36d2b9d4
Anson VanDoren <anson.vandoren@gmail.com>
2022-08-24 07:28:49 -0700

    alacritty & p10b

--- installStuff.sh
+++ installStuff.sh
@@ -1,5 +1,10 @@
-sudo pacman -S neovim exa dog curlie fd bat alacritty qutebrowser jq unzip fzf pv hunspell
+sudo pacman -S neovim exa dog curlie fd bat alacritty qutebrowser jq unzip fzf pv hunspell ranger
 yay -S gotop tre-command 1password wlogout
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing Powerlevel 10K"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "TODO:"
 echo "- Install 1Password CLI: https://developer.1password.com/docs/cli/get-started#install"
