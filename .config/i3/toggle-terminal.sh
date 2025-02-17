86fd65bf83129197e9dfe1bf8081596985a4ec51
Anson VanDoren <anson.vandoren@gmail.com>
2025-02-17 11:26:02 -0800

    better toggle terminal

--- .config/i3/toggle-terminal.sh
+++ .config/i3/toggle-terminal.sh
@@ -11,11 +11,27 @@ _hide_kitty() {
   i3-msg -q "[class=\"$DROPDOWN_CLASS\"] move scratchpad"
 }
 
-if xdotool search --limit 1 --class $DROPDOWN_CLASS 1>/dev/null 2>/dev/null; then
-  if ! _show_kitty; then
+_focus_kitty() {
+  i3-msg -q "[class=\"$DROPDOWN_CLASS\"] focus"
+}
+
+# Get the window ID of the currently focused window
+FOCUSED_WINDOW_ID=$(xdotool getwindowfocus)
+
+# Get the window ID of the kitty window (if it exists)
+KITTY_WINDOW_ID=$(xdotool search --limit 1 --class "$DROPDOWN_CLASS")
+
+if [ -n "$KITTY_WINDOW_ID" ]; then
+  # If the kitty window exists
+  if [ "$FOCUSED_WINDOW_ID" = "$KITTY_WINDOW_ID" ]; then
+    # If the kitty window is already focused, hide it
     _hide_kitty
+  else
+    # If the kitty window is not focused, focus it
+    _focus_kitty
   fi
 else
+  # If the kitty window doesn't exist, launch it
   kitty --config $HOME/.config/kitty/kitty-floating.conf --single-instance --class $DROPDOWN_CLASS 1>/dev/null 2>/dev/null &
   if [ "$1" = "--startup" ]; then
     # Wait until kitty window is ready
